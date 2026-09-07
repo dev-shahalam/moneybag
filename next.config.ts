85c82a0b800412885ba7ad9937f6a663165fedb4
Md Shah Alam <dev.mdshahalam@gmail.com>
2026-09-07 16:02:15 +0600

    Remove pwa plugin to fix turbopack build error

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,4 @@
-const nextPWA = require('next-pwa');
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-const withPWA = nextPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-});
-
-const nextConfig = {
-  // Turbopack কনফ্লিক্ট দূর করার জন্য এই লাইনটি যুক্ত করা হলো
-  turbopack: {},
-};
-
-export default withPWA(nextConfig);
+export default nextConfig;
